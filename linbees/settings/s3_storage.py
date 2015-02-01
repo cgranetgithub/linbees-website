608edcbc7393f488307d771cbe76f31d68bc93b7
charles <c.granet@gmail.com>
2015-02-01 22:58:59 +0100

    fix static url with s3

--- linbees/settings/s3_storage.py
+++ linbees/settings/s3_storage.py
@@ -1,8 +1,5 @@
 import os
 
-MEDIA_ROOT = '/media/' 
-STATIC_ROOT = '/static/'
-
 # Amazon S3 credentials
 AWS_ACCESS_KEY_ID       = os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY   = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
@@ -14,8 +11,8 @@ S3_URL = 'http://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Static files location
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATIC_URL = S3_URL + STATIC_ROOT
+STATIC_URL = S3_URL
 
 # Default File storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-MEDIA_URL = S3_URL + MEDIA_ROOT
+MEDIA_URL = S3_URL
