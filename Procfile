0b589263df0bf505a25612ef17b3b97ceb2f9ce9
charles <c.granet@gmail.com>
2015-02-01 19:17:42 +0100

    add missing config

---
+++ Procfile
@@ -0,0 +1,2 @@
+# web: gunicorn service.wsgi
+web: newrelic-admin run-program gunicorn linbees.wsgi
--- linbees/settings/prod.py
+++ linbees/settings/prod.py
@@ -37,6 +37,9 @@ WSGI_APPLICATION = 'linbees.wsgi.application'
 import dj_database_url
 DATABASES = {'default' :  dj_database_url.config() }
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 LANGUAGE_CODE = 'en'
